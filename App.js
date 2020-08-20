8828ee7acfb4311983d72bb376d0b09bb8621869
taeyoung <xoxodudwkd@gmail.com>
2020-08-20 14:22:58 +0900

    Setting preloading images and fonts

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, Image } from 'react-native';
+import { AppLoading } from 'expo';
+import { Asset } from 'expo-asset';
+import { Ionicons } from '@expo/vector-icons';
+import * as Font from 'expo-font';
+
+// args : image array
+const cacheImages = (images) =>
+  images.map((image) => {
+    if (typeof image === 'string') {
+      // URL
+      return Image.prefetch(image);
+    } else {
+      // File
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+
+const cacheFonts = (fonts) =>
+  fonts.map((font) => {
+    return Font.loadAsync(font);
+  });
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  const [isReady, setIsReady] = useState(false);
+  const handleFinish = () => setIsReady(true);
+  const loadAsync = async () => {
+    const images = [
+      require('./assets/loadingBg.jpeg'),
+      'https://www.clipartmax.com/png/middle/114-1149625_executive-airbnb-cleaning-airbnb-logo-png.png',
+    ];
+    const fonts = [Ionicons.font];
+    promiseImages = cacheImages(images);
+    promiseFonts = cacheFonts(fonts);
+    return Promise.all(...promiseImages, ...promiseFonts);
+  };
+  return isReady ? (
+    <View>
+      <Text>I'm Ready</Text>
     </View>
+  ) : (
+    <AppLoading
+      onError={console.error}
+      onFinish={handleFinish}
+      startAsync={loadAsync}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
